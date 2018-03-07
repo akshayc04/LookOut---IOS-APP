24d96c6517b7c80d0e2f05ee810bc5b800d83396
Akshay <akshay@Akshays-Mac.local>
2018-03-06 20:38:08 -0800

    modify add button for choosing pic-helper

---
+++ LookOut/Controllers/MainTabBarController.swift
@@ -0,0 +1,34 @@
+//
+//  MainTabBarController.swift
+//  LookOut
+//
+//  Created by Akshay on 3/6/18.
+//  Copyright © 2018 Akshay. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    
+    let photoHelper = PhotoHelper()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // 1
+        delegate = self
+        // 2
+        tabBar.unselectedItemTintColor = .black
+    }
+}
+extension MainTabBarController: UITabBarControllerDelegate {
+    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+        if viewController.tabBarItem.tag == 1 {
+            photoHelper.presentActionSheet(from: self)
+            return false
+        }
+        
+        return true
+    }
+}
---
+++ LookOut/Helpers/PhotoHelper.swift
@@ -0,0 +1,55 @@
+//
+//  PhotoHelper.swift
+//  LookOut
+//
+//  Created by Akshay on 3/6/18.
+//  Copyright © 2018 Akshay. All rights reserved.
+//
+
+import UIKit
+
+class PhotoHelper: NSObject {
+    // MARK: - Properties
+    
+    var completionHandler: ((UIImage) -> Void)?
+    
+    // MARK: - Helper Methods
+    
+    func presentActionSheet(from viewController: UIViewController) {
+        let alertController = UIAlertController(title: nil, message: "Where do you want to get your picture from?", preferredStyle: .actionSheet)
+        
+        // 2
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            let capturePhotoAction = UIAlertAction(title: "Take Photo", style: .default, handler: { [unowned self] action in
+                self.presentImagePickerController(with: .camera, from: viewController)
+            })
+            
+            alertController.addAction(capturePhotoAction)
+        }
+        
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            let uploadAction = UIAlertAction(title: "Upload from Library", style: .default, handler: { [unowned self] action in
+                self.presentImagePickerController(with: .photoLibrary, from: viewController)
+            })
+            
+            alertController.addAction(uploadAction)
+        }
+        
+        // 6
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alertController.addAction(cancelAction)
+        
+        // 7
+        viewController.present(alertController, animated: true)
+        
+    }
+    
+    
+    //func to image picker
+    func presentImagePickerController(with sourceType: UIImagePickerControllerSourceType, from viewController: UIViewController) {
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = sourceType
+        
+        viewController.present(imagePickerController, animated: true)
+    }
+}
